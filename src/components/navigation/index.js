47f56bc65d71ed9f3d67435dcef3b71555f7e33c
kaka <kakaxsena@gmail.com>
2025-07-23 14:44:21 -0300

    vite and tsconfig

---
+++ src/components/navigation/index.js
@@ -0,0 +1,5 @@
+// src/components/layout/navigation/index.js
+export { default as Navigation } from './Navigation.vue'
+export { default as NavLogo } from './NavLogo.vue'
+export { default as NavList } from './NavList.vue'
+export { default as NavItem } from './NavItem.vue'
