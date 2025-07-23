42be227a12f90671c3de2cb75a7ca52723d7f666
kaka <kakaxsena@gmail.com>
2025-07-23 15:49:02 -0300

    dashboard

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Navigation } from '@/components/navigation'
+</script>
+
+<template>
+  <div class="flex min-h-screen">
+    <Navigation />
+    <main class="flex-1 p-6 ml-64">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
