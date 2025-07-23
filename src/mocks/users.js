1f846848a41ffa0ad9be47c35504bded52f4e2c2
kaka <kakaxsena@gmail.com>
2025-07-23 17:38:16 -0300

    users mockup data

---
+++ src/mocks/users.js
@@ -0,0 +1,4 @@
+export const userMockData = [
+  { id: 1, name: 'John Doe', document: '123456789', status: 'active' },
+  { id: 2, name: 'Jane Smith', document: '987654321', status: 'inactive' },
+]
