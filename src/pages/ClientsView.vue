57891fb2be644d592197c4b30faa8140575cf78d
kaka <kakaxsena@gmail.com>
2025-07-23 15:50:06 -0300

    clients movies and rental view squeleton

---
+++ src/pages/ClientsView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">Clients</h1>
+    <p class="text-gray-600">Clients content will go here</p>
+  </div>
+</template>
+
+<script setup>
+// TODO
+</script>
---
+++ src/pages/RentalsView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">Rentals</h1>
+    <p class="text-gray-600">Rentals content will go here</p>
+  </div>
+</template>
+
+<script setup>
+// TODO
+</script>
