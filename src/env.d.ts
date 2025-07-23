7b9f56d54715ae81662c3b143f66addadd09a4a4
kaka <kakaxsena@gmail.com>
2025-07-23 08:38:45 -0300

    ran formart

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vue-router" />
 
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
-declare module "*.css" {
-  const css: { [key: string]: string };
-  export default css;
+declare module '*.css' {
+  const css: { [key: string]: string }
+  export default css
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
-import LoginForm from "../components/LoginForm.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginForm from '../components/LoginForm.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/",
-      name: "login",
+      path: '/',
+      name: 'login',
       component: LoginForm,
     },
   ],
-});
+})
 
-export default router;
+export default router
