9326ff8e2c187ce58ed351ad7da74da5bbcf8f0a
kaka <kakaxsena@gmail.com>
2025-07-23 08:37:54 -0300

    added prettier and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended', // only if using TypeScript
+    'plugin:prettier/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off',
+  },
+}
