b97862fff1925bbef4b0812f960beca8bf0db4dd
kaka <kakaxsena@gmail.com>
2025-07-23 16:15:52 -0300

    changing route name to customer

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginForm from '../components/LoginForm.vue'
+import LoginForm from '@/pages/LoginForm.vue'
+import Dashboard from '@/pages/Dashboard.vue'
+import UsersView from '@/pages/UsersView.vue'
+import CustomerView from '@/pages/CustomerView.vue'
+import MoviesView from '@/pages/MoviesView.vue'
+import RentalsView from '@/pages/RentalsView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +13,34 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginForm,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      redirect: '/dashboard/movies',
+      children: [
+        {
+          path: 'movies',
+          component: MoviesView,
+          name: 'movies',
+        },
+        {
+          path: 'users',
+          component: UsersView,
+          name: 'users',
+        },
+        {
+          path: 'clients',
+          component: CustomerView,
+          name: 'clients',
+        },
+        {
+          path: 'rentals',
+          component: RentalsView,
+          name: 'rentals',
+        },
+      ],
     },
   ],
 })
