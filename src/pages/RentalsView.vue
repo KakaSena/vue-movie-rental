8305cad6f532a7e81bc470cf16e0dd2c821f0aa9
kaka <kakaxsena@gmail.com>
2025-07-24 16:26:06 -0300

    added filter for rented and delivered movies

--- src/pages/RentalsView.vue
+++ src/pages/RentalsView.vue
@@ -1,10 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+import SearchInput from '@/components/search/SearchInput.vue'
+import SelectInput from '@/components/search/SelectInput.vue'
+import Button from '@/components/ui/Button.vue'
+import { statusRentalOptions } from '@/constants/statusOptions'
+
+const searchTerm = ref('')
+const statusFilter = ref('all')
+</script>
+
 <template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Rentals</h1>
-    <p class="text-gray-600">Rentals content will go here</p>
+  <div class="space-y-6">
+    <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
+      <div class="flex-1">
+        <SearchInput v-model="searchTerm" placeholder="Search by name or document" />
+      </div>
+      <div class="flex items-center gap-4">
+        <SelectInput v-model="statusFilter" :options="statusRentalOptions" class="w-40" />
+        <Button @click="openDialog()">Add User</Button>
+      </div>
+    </div>
   </div>
 </template>
-
-<script setup>
-// TODO
-</script>
