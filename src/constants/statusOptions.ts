6eb5605ff004980f7e7825a19e9c670a49140a92
kaka <kakaxsena@gmail.com>
2025-07-24 16:24:23 -0300

    defined a generic interface so we can also use it to filter rented movies

--- src/constants/statusOptions.ts
+++ src/constants/statusOptions.ts
@@ -1,7 +1,13 @@
-import type { StatusOption } from '@/types/select'
+import type { SelectRentalOption, StatusUsersOption } from '@/types/select'
 
-export const statusOptions: readonly StatusOption[] = [
+export const statusUserOptions: readonly StatusUsersOption[] = [
   { value: 'all', label: 'All Status' },
   { value: 'active', label: 'Active' },
   { value: 'inactive', label: 'Inactive' },
+] as const
+
+export const statusRentalOptions: readonly SelectRentalOption[] = [
+  { value: 'all', label: 'All Status' },
+  { value: 'rented', label: 'Rented' },
+  { value: 'delivered', label: 'Delivered' },
 ] as const
--- src/types/select.ts
+++ src/types/select.ts
@@ -1,10 +1,11 @@
-export interface SelectOption {
-  value: string
+// Base interface with generic type
+export interface SelectOption<T = string> {
+  value: T
   label: string
 }
 
-export type StatusOptionValue = 'all' | 'active' | 'inactive'
+export type StatusUsersValue = 'all' | 'active' | 'inactive'
+export type StatusRentalValue = 'all' | 'rented' | 'delivered'
 
-export interface StatusOption extends SelectOption {
-  value: StatusOptionValue
-}
+export type StatusUsersOption = SelectOption<StatusUsersValue>
+export type StatusRentalOption = SelectOption<StatusRentalValue>
