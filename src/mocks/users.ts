51cdb5bd8e71632b3e37982ed66ea04e830253e4
kaka <kakaxsena@gmail.com>
2025-07-25 01:59:12 -0300

    updated mock to have password

--- src/mocks/users.ts
+++ src/mocks/users.ts
@@ -1,6 +1,6 @@
 import type { User } from '@/types/user'
 
 export const userMockData: User[] = [
-  { id: 1, name: 'John Doe', document: '123456789', status: 'active' },
-  { id: 2, name: 'Jane Smith', document: '987654321', status: 'inactive' },
+  { id: 1, name: 'Admin', document: '123456789', password: 'admin123', status: 'active' },
+  { id: 2, name: 'Disabled user', document: '123456789', password: 'admin123', status: 'inactive' },
 ]
