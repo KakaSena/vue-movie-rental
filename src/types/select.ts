5d3dff4be0c373276454001e04f1948324fd242b
kaka <kakaxsena@gmail.com>
2025-07-25 01:15:29 -0300

    types for rental with proper name

--- src/types/select.ts
+++ src/types/select.ts
@@ -1,11 +1,10 @@
-// Base interface with generic type
 export interface SelectOption<T = string> {
   value: T
   label: string
 }
 
 export type StatusUsersValue = 'all' | 'active' | 'inactive'
-export type StatusRentalValue = 'all' | 'rented' | 'delivered'
+export type StatusRentalValue = 'all' | 'rented' | 'returned'
 
 export type StatusUsersOption = SelectOption<StatusUsersValue>
 export type StatusRentalOption = SelectOption<StatusRentalValue>
