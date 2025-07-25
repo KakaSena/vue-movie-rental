fb67947259943c034a094efcd9e59e77add0707c
kaka <kakaxsena@gmail.com>
2025-07-25 03:42:23 -0300

    created my user to easy acess

--- src/mocks/users.ts
+++ src/mocks/users.ts
@@ -3,4 +3,5 @@ import type { User } from '@/types/user'
 export const userMockData: User[] = [
   { id: 1, name: 'Admin', document: '123456789', password: 'admin123', status: 'active' },
   { id: 2, name: 'Disabled user', document: '1234579', password: 'admin123', status: 'inactive' },
+  { id: 2, name: 'kaka', document: '12345', password: '123', status: 'active' },
 ]
